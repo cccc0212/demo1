775892c2851a207bb23f2b2b3cf68453006e1c42
cccc0212 <835926954@qq.com>
2018-09-26 15:21:42 +0800

    test

---
+++ src/main/java/cache/CacheTest.java
@@ -0,0 +1,24 @@
+package cache;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CacheTest {
+    public static void main(String[] args) {
+
+        MapCacheManager cache = MapCacheManager.getInstance();
+        Map<String, String> cacheMap = new ConcurrentHashMap<String, String>();
+
+        cacheMap = cache.getMapCache();
+        Set<String> set = cacheMap.keySet();
+        Iterator<String> it = set.iterator();
+
+        while(it.hasNext()){
+            String key = it.next();
+            System.out.println(key+"="+cacheMap.get(key));
+        }
+    }
+
+}
---
+++ src/main/java/demo/test1.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class test1 {
+
+    public static void main(String[] args){
+        String str = "        ";
+        System.out.println("=============" + str.length());
+        System.out.println("=============" + str.trim().length());
+    }
+}
