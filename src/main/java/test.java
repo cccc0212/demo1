fc3a8caa100618bf1b3b056303c0dcb9006623b3
cccc0212 <835926954@qq.com>
2018-09-30 15:14:34 +0800

    demo

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,5 +3,10 @@ public class test {
         String str1= "";
         String[] str = str1.split(",");
         System.out.print(str.length);
+
+        String str2 = "Hello World      ";
+        String str3 = str2.trim();
+        System.out.println(str2);
+        System.out.println(str3);
      }
 }
